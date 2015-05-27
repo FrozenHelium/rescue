2d8dd863c754057ade44b164e4afbdd76e1831bb
Slesa Adhikari <artycrab_slesh@yahoo.com>
2015-05-27 17:39:19 +0545

    Organizations tab filled

---
+++ views/info_organizations_view.php
@@ -0,0 +1,173 @@
+<head>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+</head>
+<!--<style>
+  body {
+      position: relative;
+  }
+  ul.nav-pills {
+      top: 20px;
+      position: relative;
+  }
+  div.col-sm-9 div {
+      height: 250px;
+      font-size: 28px;
+  }
+  #section1 {color: #fff; background-color: #1E88E5;}
+  #section2 {color: #fff; background-color: #673ab7;}
+  #section3 {color: #fff; background-color: #ff9800;}
+  #section4 {color: #fff; background-color: #00bcd4;}
+  #section5 {color: #fff; background-color: #009688;}
+  #section6 {color: #fff; background-color: #00fdff;}
+  
+  @media screen and (max-width: 810px) {
+    #section1, #section2, #section3, #section4, #section5, #section6  {
+        margin-left: 150px;
+    }
+  }
+  </style>
+<div class="jumbotron">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-11">
+                <h1>Organizations</h1>
+                <p><strong> Humanitarian organizations</strong> working for Rescue</p>
+            </div>
+            <div class="col-md-1">
+                <a href="index.php?page=home">Home</a>
+            </div>
+        </div>
+     </div>
+</div>
+
+<div class="container">
+  <div class="row">
+    <nav class="col-sm-3" id="myScrollspy">
+      <ul class="nav nav-pills nav-stacked">
+        <li class="active"><a href="#section1">Maiti Nepal</a></li>
+        <li><a href="#section2">ABC Nepal</a></li>
+        <li><a href="#section3">Change Nepal</a></li>
+        <li><a href="#section4"><abbr title="National Network Against Girl Trafficking">NNAGT</abbr></a></li>
+        <li><a href="#section5"><abbr title="Alliance Against Trafficking of Women and Children in Nepal">AATWIN</abbr></a></li>
+        <li><a href="#section6">Childreach Nepal</a></li>
+      </ul>
+    </nav>
+    <div class="col-sm-9">
+      <div id="section1">    
+        <h1>Maiti Nepal</h1>
+        <p>Maiti Nepal (Nepali: माइती नेपाल) is a non-profit organization in Nepal dedicated to helping victims of sex trafficking. Currently, it operates a rehabilitation home in Kathmandu, as well as transit homes at the Indo-Nepal border towns, preventive homes in the countryside and an academy in Kathmandu .
+        Maiti Nepal’s Centers[edit]
+Maiti Nepal has three prevention homes, eleven transit homes, a hospices and Teresa Academy.[6] The data for the centres of Maiti Nepal has been taken from the website of South Asia Foundation.[7]
+Transit Homes[edit]
+The transit homes of Maiti Nepal are located in ten different places in Nepal. These are in...
+Pashupatinagar,Illam
+Kakarvita,Jhapa
+Biratnagar,Morang
+Birgunj,Parsa
+Bhairawa,Rupandehi
+Nepalgunj,Bake
+Dhangadhi,Kailali
+Kathmandu, Thankot
+Maheshpur - Nawalparasi
+Mahendranagar
+Rehabilitation Centers[edit]
+The two rehabilitation centres of Maiti Nepal are located in Kathmandu and Ithari.
+Women Rehabilitation Centre, Kathmandu
+Women Rehabilitation Centre, Ithari
+Prevention Homes[edit]
+Chisapaani - Nuwakot
+Bardaghat - Nawalparasi
+Hetauda – Makwanpur
+Hospice[edit]
+Satighatta - Jhapa District Eastern Nepal</p>
+      </div>
+      <div id="section2"> 
+        <h1>ABC Nepal</h1>
+        <p>Try to scroll this section and look at the navigation list while scrolling!</p>
+      </div>        
+      <div id="section3">         
+        <h1>Change Nepal</h1>
+        <p>Try to scroll this section and look at the navigation list while scrolling!</p>
+      </div>
+      <div id="section4">         
+        <h1><abbr title="National Network Against Girl Trafficking">NNAGT</abbr></h1>
+        <p>Try to scroll this section and look at the navigation list while scrolling!</p>
+      </div>      
+      <div id="section5">         
+        <h1><abbr title="Alliance Against Trafficking of Women and Children in Nepal">AATWIN</abbr></h1>
+        <p>Try to scroll this section and look at the navigation list while scrolling!</p>
+      </div>
+      <div id="section6">         
+        <h1>Childreach Nepal</h1>
+        <p>Try to scroll this section and look at the navigation list while scrolling!</p>
+      </div>
+    </div>
+  </div>
+</div>
+-->
+<div class="container">
+  <h2>Organizations</h2>
+  <p>Humanitarian organisations helping in Rescue</p>
+  <ul class="nav nav-tabs">
+    <li class="active"><a href="#menu1">Maiti Nepal</a></li>
+    <li><a href="#menu2">ABC Nepal</a></li>
+    <li><a href="#menu3">Change Nepal</a></li>
+    <li><a href="#menu4"><abbr title="National Network Against Girl Trafficking">NNAGT</abbr></a></li>
+    <li><a href="#menu5"><abbr title="Alliance Against Trafficking of Women and Children in Nepal">AATWIN</abbr></a></li>
+    <li><a href="#menu6">Childreach Nepal</a></li>
+  </ul>
+
+  <div class="tab-content">
+    <div id="menu1" class="tab-pane fade in active">
+      <h3>Maiti Nepal</h3>
+      <p>Maiti Nepal (Nepali: माइती नेपाल) is a non-profit organization in Nepal dedicated to helping victims of sex trafficking. Currently, it operates a rehabilitation home in Kathmandu, as well as transit homes at the Indo-Nepal border towns, preventive homes in the countryside and an academy in Kathmandu.</p>
+      <p><strong>Contact us</strong><br>
+        Maiti Nepal<br>
+        83-Maiti Marg, Pingalsthan, Gaushala<br>
+        P.O. Box 9599, Kathmandu, Nepal<br>
+        Phone: +977-1-4492904<br>
+        Fax: +977-1-4489978<br>
+        e-mail: info@maitinepal.org, program@maitinepal.org<br>
+      </p>
+      <ul class="nav nav-tabs">
+      <li><a href="http://www.maitinepal.org/">Official Website</a></li>
+      <li><a href="http://en.wikipedia.org/wiki/Maiti_Nepal">Wiki Link</a></li>
+      </ul>
+    </div>
+    <div id="menu2" class="tab-pane fade">
+      <h3>ABC Nepal</h3>
+      <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+    </div>
+    <div id="menu3" class="tab-pane fade">
+      <h3>Change Nepal</h3>
+      <p>Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+    </div>
+    <div id="menu4" class="tab-pane fade">
+      <h3><abbr title="National Network Against Girl Trafficking">NNAGT</abbr></h3>
+      <p>Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+    </div>
+    <div id="menu5" class="tab-pane fade">
+      <h3><abbr title="Alliance Against Trafficking of Women and Children in Nepal">AATWIN</abbr></h3>
+      <p>Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+    </div>
+    <div id="menu6" class="tab-pane fade">
+      <h3>Childreach Nepal</h3>
+      <p>Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+    </div>
+  </div>
+</div>
+
+<script>
+$(document).ready(function(){
+    $(".nav-tabs a").click(function(){
+        $(this).tab('show');
+    });
+    $('.nav-tabs a').on('shown.bs.tab', function(event){
+        var x = $(event.target).text();         // active tab
+        var y = $(event.relatedTarget).text();  // previous tab
+        $(".act span").text(x);
+        $(".prev span").text(y);
+    });
+});
+</script>
