a4d0f44efd84adc8b51be5b16a45cdc71607eb46
AdityaKhatri <adityakhatri47@gmail.com>
2015-05-28 00:22:12 +0545

    map added on button

--- views/urgent_view.php
+++ views/urgent_view.php
@@ -12,6 +12,19 @@
      </div>
 </div>
 
+<script src="http://maps.googleapis.com/maps/api/js"></script>
+<script>
+function initialize() {
+  var mapProp = {
+    center:new google.maps.LatLng(51.508742,-0.120850),
+    zoom:5,
+    mapTypeId:google.maps.MapTypeId.ROADMAP
+  };
+  var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
+google.maps.event.addDomListener(window, 'load', initialize);
+</script>
+
 <div class="container">
     <form class="form-urgent" action="index.php?page=urgent" method="post" name="urgent_form" role="form">
         <div class="row">
@@ -19,7 +32,28 @@
                 <input type="text" class="form-control" placeholder="Location" name="location" id="location" required/>
             </div>
             <div class="col-md-3">
-                <input type="button" class="btn btn-lg btn-primary btn-block" value="Select from Map"/>
+                <div class="map-modal">
+                    <!-- Button (to Trigger Modal) -->
+                   <button type="button" class="btn btn-lg btn-primary btn-block" data-toggle="modal" data-target="#myModal">Select from Mappy</button>      
+                    <!-- Modal HTML -->
+                    <div id="myModal" class="modal fade">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                    <h4 class="modal-title">Select Location</h4>
+                                </div>
+                                <div class="modal-body">
+                                    <p>Are you sure about this location?</p>
+                                    <div id="googleMap" style="width:540px;height:380px;"></div>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-lg btn-primary btn-block">Save changes</button>                                
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div> 
             </div>
         </div>
         <div class="row">
