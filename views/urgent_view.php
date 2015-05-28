35c9342f63a8e462c11c98de816c35d1b4ec396e
AdityaKhatri <adityakhatri47@gmail.com>
2015-05-28 09:54:58 +0545

    Map

--- views/urgent_view.php
+++ views/urgent_view.php
@@ -1,4 +1,3 @@
-
 <div class="jumbotron">
     <div class="container">
         <div class="row">
@@ -12,20 +11,6 @@
         </div>
      </div>
 </div>
-
-<script src="http://maps.googleapis.com/maps/api/js"></script>
-<script>
-function initialize() {
-  var mapProp = {
-    center:new google.maps.LatLng(51.508742,-0.120850),
-    zoom:5,
-    mapTypeId:google.maps.MapTypeId.ROADMAP
-  };
-  var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
-}
-google.maps.event.addDomListener(window, 'load', initialize);
-</script>
-
 <div class="container">
     <form class="form-urgent" action="index.php?page=urgent" method="post" name="urgent_form" role="form">
         <div class="row">
@@ -46,7 +31,7 @@ google.maps.event.addDomListener(window, 'load', initialize);
                                 </div>
                                 <div class="modal-body">
                                     <p>Are you sure about this location?</p>
-                                    <div id="googleMap" style="width:540px;height:380px;"></div>
+                                    <iframe src="map_view.php" width="560px" height="400px"></iframe>
                                 </div>
                                 <div class="modal-footer">
                                     <button type="button" class="btn btn-lg btn-primary btn-block">Save changes</button>                                
