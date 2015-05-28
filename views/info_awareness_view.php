17985b919d6bc6696ad92cbc0adc65cb826994cd
Slesa Adhikari <artycrab_slesh@yahoo.com>
2015-05-28 19:36:46 +0545

    doodle added

--- views/info_awareness_view.php
+++ views/info_awareness_view.php
@@ -18,7 +18,7 @@
 			<!-- Wrapper for slides -->
 			<div class="carousel-inner" role="listbox">
 				<div class="item active">
-					<img src="img1.jpg" alt="first">
+					<img src="../public/imgs/img1.png" alt="first">
 					<div class="carousel-caption">
 					</div>
 				</div>
